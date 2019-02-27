da95bc32b7d046e1282516cea027ed718c6d4a9b
Jonas Millan <Jonas_Millan@intranet.itresources>
2019-02-26 21:17:34 -0300

    servidor culminado

---
+++ clase2/server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const app = http.createServer((req, res) =>{
+
+})
+
+app.listen(8000)
+
+const persona = {
+    nombre: 'pepe',
+    apellido: 'paco'
+}
+console.log(persona)
